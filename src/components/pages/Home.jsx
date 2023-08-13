76c0c4de4c4a16529cf81bb0c9145216160b8719
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-13 17:55:29 -0400

    delete file

--- src/components/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-    return(
-        <div>
-            <h1 className="grid mt-60 justify-items-center text-center text-2xl">Hello World</h1>
-        </div>
-    )
-}
-
-export { Home }
--- src/routers/Routs.jsx
+++ src/routers/Routs.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import { Character } from "../components/pages/Character";
 import { Location } from "../components/pages/Location";
 import { Episode } from "../components/pages/Episode";
-import { Home } from "../components/pages/Home";
 import { NotFound } from "../components/pages/Notfound";
 
 function Routs() {
@@ -11,8 +10,7 @@ function Routs() {
     return(
         <header>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/character" element={<Character/>}/>
+                <Route path="/" element={<Character/>}/>
                 <Route path="/location" element={<Location/>}/>
                 <Route path="/episode" element={<Episode/>}/>
                 <Route path="*" element={<NotFound/>}/>
