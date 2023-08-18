89bb7a9eee41dd669bb60c06bc9ed7b81aa8f542
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-18 08:29:32 -0400

    modified style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 // component for each character
 function Card(data) {
     return(
-        <div className="flex flex-col items-center p-3 mt-4 cursor-pointer">
-            <figure className="w-56 h-56">
+        <div className="flex flex-col items-center p-3 mt-4">
+            <figure className="w-56 h-56 cursor-pointer">
                 <img className="w-full rounded-full justify-center border-2 border-black" src={data.data.image}/>
             </figure>
             <div className="mt-2">
