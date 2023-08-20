94bb738c31381ee66a772b0ad0859ac11963df84
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-08-19 23:06:35 -0400

    character responsive

--- src/components/pages/Character.jsx
+++ src/components/pages/Character.jsx
@@ -8,9 +8,9 @@ function Character() {
 
     return(
         <div>
-            <h1 className="grid mt-32 justify-items-center text-center text-2xl">Characters</h1>
+            <h1 className="grid mt-44 justify-items-center text-center text-2xl">Characters</h1>
 
-            <div className="grid justify-center grid-cols-3">
+            <div className="grid justify-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5*/">
                 {character?.map(data => <Card key={data.id} data={data}/>)}
             </div>
         </div>
