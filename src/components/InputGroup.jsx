9caa8e1d9e9b20b4940a250cf9cf85674d1b5e4c
Yilbert Luna <yilbert.luna.e@gmail.com>
2023-09-10 18:32:49 -0400

    modified file

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -6,7 +6,7 @@ function InputGroup({name, changeId, total}) {
     return(
         <div className="grid justify-items-center mt-5 mb-10">
             <select className="border border-black rounded-md w-72 sm:w-96 h-8 sm:h-11 sm:text-lg text-center focus:outline-none" onChange={(e) => changeId(e.target.value)}  id="name">
-                <option value="1">Locations...</option>
+                <option value="1">{name}...</option>
                 {[...Array(total).keys()].map((x, index) => {
                     return(
                         <option key={index} value={x + 1}>
